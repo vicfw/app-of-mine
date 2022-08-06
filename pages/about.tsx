66a7c0a4d97408a3b9b74e38881efc9eaa0e4a6b
Farid <faridforward222@gmail.com>
2022-08-06 13:26:50 +0430

    initial commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,6 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Link from '../src/Link';
 import ProTip from '../src/ProTip';
 import Copyright from '../src/Copyright';
 
@@ -24,7 +23,7 @@ const About: NextPage = () => {
           MUI v5 + Next.js with TypeScript example
         </Typography>
         <Box maxWidth="sm">
-          <Button variant="contained" component={Link} noLinkStyle href="/">
+          <Button variant="contained" noLinkStyle href="/">
             Go to the home page
           </Button>
         </Box>
