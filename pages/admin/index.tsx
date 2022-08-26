4de9a15a6538b5ad94ff6e50127b2e67a5bb6688
farid <faridforward222@gmail.com>
2022-08-26 21:10:21 +0430

    s

---
+++ pages/admin/index.tsx
@@ -0,0 +1,31 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import { FC } from "react";
+import AdminLayout from "../../src/components/Layout/";
+
+const Index: FC<any> = ({}) => {
+  return <AdminLayout></AdminLayout>;
+};
+export default Index;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  try {
+    const session = await getSession({ req });
+
+    if (session?.user.isAdmin) {
+      return {
+        props: {},
+      };
+    } else {
+      return {
+        props: {},
+        redirect: {
+          destination: "/admin/login",
+          permanent: false,
+        },
+      };
+    }
+  } catch (e) {
+    return { props: {} };
+  }
+};
