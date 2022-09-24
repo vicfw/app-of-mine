ae45fc7255cb9e4760d13eacac5fa3b5f1304c73
Farid <61201661+vicfw@users.noreply.github.com>
2022-09-24 18:31:27 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+   env: {
+    MONGODB_URI:
+      'mongodb+srv://farid:farid2560714@cluster0.axthw.mongodb.net/truck-app?retryWrites=true&w=majority',
+    NEXTAUTH_URL: 'https://master.d2emqtezw7d45.amplifyapp.com',
+    NEXTAUTH_SECRET: 'asdasdasfdkjioqjgiojoi213123',
+  },
 };
