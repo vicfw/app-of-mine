01640241af7b5644f16cb90c41d8751113b8fae9
Farid <faridforward222@gmail.com>
2022-09-25 11:08:18 +0330

    node env set

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
 module.exports = {
   reactStrictMode: false,
   env: {
+    NODE_ENV: 'development',
     MONGODB_URI:
       'mongodb+srv://farid:farid2560714@cluster0.axthw.mongodb.net/truck-app?retryWrites=true&w=majority',
-    NEXTAUTH_URL: 'http://localhost:3000',
+    NEXTAUTH_URL:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : 'https://master.d2emqtezw7d45.amplifyapp.com',
     NEXTAUTH_SECRET: 'asdasdasfdkjioqjgiojoi213123',
   },
 };
