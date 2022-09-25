467024c26362a24bd202044f5170f607b4da5bd1
Farid <faridforward222@gmail.com>
2022-09-25 10:59:24 +0330

    ss

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  env: {
+    MONGODB_URI:
+      'mongodb+srv://farid:farid2560714@cluster0.axthw.mongodb.net/truck-app?retryWrites=true&w=majority',
+    NEXTAUTH_URL: 'http://localhost:3000',
+    NEXTAUTH_SECRET: 'asdasdasfdkjioqjgiojoi213123',
+  },
 };
