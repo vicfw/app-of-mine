5388230583eb2b6d1d6a3e3608f7140b55e7cdf3
Farid <faridforward222@gmail.com>
2022-09-26 15:33:33 +0330

    aws s3 file upload worked

--- next.config.js
+++ next.config.js
@@ -8,9 +8,12 @@ module.exports = {
     ],
   },
   env: {
-    NEXTAUTH_URL:
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3000'
-        : 'https://master.d2emqtezw7d45.amplifyapp.com',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    MONGODB_URI: process.env.MONGODB_URI,
+    S3_UPLOAD_BUCKET: process.env.S3_UPLOAD_BUCKET,
+    S3_UPLOAD_KEY: process.env.S3_UPLOAD_KEY,
+    S3_UPLOAD_REGION: process.env.S3_UPLOAD_REGION,
+    S3_UPLOAD_SECRET: process.env.S3_UPLOAD_SECRET,
   },
 };
